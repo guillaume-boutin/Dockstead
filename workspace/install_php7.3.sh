c255cf362f3918def0bddce1b611779870b53044
Guillaume Boutin <guillaumeboutin2101@gmail.com>
2020-10-08 22:32:08 -0400

    Initial commit. Basic setup with php 7.2 to 7.4

---
+++ workspace/install_php7.3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get install -y --allow-downgrades --allow-remove-essential \
+    --allow-change-held-packages \
+    php7.3-cli \
+    php7.3-common \
+    php7.3-curl \
+    php7.3-intl \
+    php7.3-json \
+    php7.3-xml \
+    php7.3-mbstring \
+    php7.3-mysql \
+    php7.3-pgsql \
+    php7.3-sqlite \
+    php7.3-sqlite3 \
+    php7.3-zip \
+    php7.3-bcmath \
+    php7.3-memcached \
+    php7.3-gd \
+    php7.3-dev
---
+++ workspace/install_php7.4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get install -y --allow-downgrades --allow-remove-essential \
+    --allow-change-held-packages \
+    php7.4-cli \
+    php7.4-common \
+    php7.4-curl \
+    php7.4-intl \
+    php7.4-json \
+    php7.4-xml \
+    php7.4-mbstring \
+    php7.4-mysql \
+    php7.4-pgsql \
+    php7.4-sqlite \
+    php7.4-sqlite3 \
+    php7.4-zip \
+    php7.4-bcmath \
+    php7.4-memcached \
+    php7.4-gd \
+    php7.4-dev
